e3894950cbbcd17e29383c289ed86555f4032357
Marcin <bee4004@gmail.com>
2019-10-19 15:14:20 +0200

    add RegisterTest

---
+++ TaskOne/taskTests/Part_1_Tests/RegisterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task_1.Part_1;
+
+namespace taskTests.Part_1Tests
+{
+    [TestClass]
+    public class RegisterTest
+    {
+        [TestMethod]
+        public void personFirstNameGetterTest()
+        {
+            Register r = new Register("Jan", "Kowalski");
+            Assert.AreEqual("Jan", r.FirstName);
+        }
+
+        [TestMethod]
+        public void personLastNameGetterTest()
+        {
+            Register r = new Register("Jan", "Kowalski");
+            Assert.AreEqual("Kowalski", r.LastName);
+        }
+
+        [TestMethod]
+        public void personStringGetterTest()
+        {
+            Register r = new Register("Jan", "Kowalski");
+            Assert.AreEqual("Jan Kowalski", r.All);
+        }
+    }
+}
