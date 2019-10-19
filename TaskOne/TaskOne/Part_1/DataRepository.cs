6b82c4eb3984100170e077a08434ff968da495da
Shumitu <darkdragon5255@gmail.com>
2019-10-19 17:16:59 +0200

    feat: constructor with parameters

--- TaskOne/TaskOne/Part_1/DataRepository.cs
+++ TaskOne/TaskOne/Part_1/DataRepository.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Task_1.Part_2;
 
 namespace Task_1.Part_1
 {
     public class DataRepository
     {
         private DataContext context;
+        private DataFill fill;
+
+        public DataRepository(DataFill fill, DataContext context)
+        {
+            this.context = context;
+            this.fill = fill;
+        }
+
+        public DataContext Context
+        {
+            get
+            {
+                return context;
+            }
+            set
+            {
+                context = value;
+            }
+        }
+
+        public DataFill Fill
+        {
+            get
+            {
+                return fill;
+            }
+            set
+            {
+                fill = value;
+            }
+        }
     }
 }
