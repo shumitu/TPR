d97ec7f3e1821d4564a69cf505bec6099daa3c73
Shumitu <darkdragon5255@gmail.com>
2019-10-26 13:10:42 +0200

    feat: new class, data service

---
+++ TaskOne/TaskOne/Part_4/DataService.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Task_1.Part_1;
+
+namespace Task_1.Part_4
+{
+    class DataService
+    {
+        private DataRepository data;
+
+
+        public DataService(DataRepository data)
+        {
+            this.data = data;
+        }
+
+
+        public void View(IEnumerable<Register> lists)
+        {
+            List<Register> new1 = lists.ToList<Register>();
+
+
+            for (int i = 0; i < lists.Count(); i++)
+            {
+                Console.WriteLine(new1[i].FullName);
+            }
+        }
+
+
+        public void View(IEnumerable<Catalog> catalogs)
+        {
+            Dictionary<int, Catalog> new1 = catalogs.ToDictionary(x => x.BookId, x => x);
+
+
+            for (int i = 0; i < catalogs.Count(); i++)
+            {
+                Console.WriteLine(new1[i].All);
+            }
+        }
+
+
+        public void View(IEnumerable<StatusDescription> descriptions)
+        {
+            List<StatusDescription> new1 = descriptions.ToList<StatusDescription>();
+
+
+            for (int i = 0; i < descriptions.Count(); i++)
+            {
+                Console.WriteLine(new1[i].All);
+            }
+        }
+
+
+        public void View(IEnumerable<Event> events)
+        {
+            ObservableCollection<Event> new1 = new ObservableCollection<Event>(events);
+
+
+            for (int i = 0; i < events.Count(); i++)
+            {
+                Console.WriteLine(new1[i].All);
+            }
+        }
+
+
+
+        public IEnumerable<StatusDescription> StatusDescriptionForCatalog(Catalog catalog)
+        {
+            List<StatusDescription> all = this.data.GetAllStatusDescriptions().ToList<StatusDescription>();
+            List<StatusDescription> new1 = new List<StatusDescription>();
+
+
+            foreach (var description in all)
+            {
+                if (description.Catalog.Equals(catalog))
+                {
+                    new1.Add(description);
+                }
+            }
+
+            return (IEnumerable<StatusDescription>)new1;
+        }
+
+
+        public IEnumerable<Event> EventForRegister(Register register)
+        {
+            ObservableCollection<Event> all = new ObservableCollection<Event>(this.data.GetAllEvents());
+            ObservableCollection<Event> new1 = new ObservableCollection<Event>();
+
+
+            foreach (var event1 in all)
+            {
+                if (event1.Person.Equals(register))
+                {
+                    new1.Add(event1);
+                }
+            }
+
+            return (IEnumerable<Event>)new1;
+        }
+
+
+        public IEnumerable<Event> EventForStatusDescription(StatusDescription description)
+        {
+            ObservableCollection<Event> all = new ObservableCollection<Event>(this.data.GetAllEvents());
+            ObservableCollection<Event> new1 = new ObservableCollection<Event>();
+
+
+            foreach (var event1 in all)
+            {
+                if (event1.Description.Equals(description))
+                {
+                    new1.Add(event1);
+                }
+            }
+
+            return (IEnumerable<Event>)new1;
+        }
+
+
+
+        public void AddRegister(Register register)
+        {
+            this.data.AddRegister(register);
+        }
+
+
+        public void AddRegister(int personId, string firstName, string lastName)
+        {
+            this.data.AddRegister(new Register(personId, firstName, lastName));
+        }
+
+
+        public void AddCatalog(Catalog catalog)
+        {
+            this.data.AddToCatalog(catalog);
+        }
+
+
+        public void AddCatalog(int bookId, string author, string title, int year)
+        {
+            this.data.AddToCatalog(new Catalog(bookId, author, title, year));
+        }
+
+
+        public void AddStatusDescription(StatusDescription description)
+        {
+            this.data.AddStatusDescription(description);
+        }
+
+
+        public void AddStatusDescription(Catalog catalog, int amount, double price, string description, DateTime date)
+        {
+            this.data.AddStatusDescription(new StatusDescription(catalog, amount, price, description, date));
+        }
+
+
+        public void AddEvent(Event event1)
+        {
+            this.data.AddEvent(event1);
+        }
+
+
+        public void AddEvent(Register person, StatusDescription description, DateTime dateBorrow, DateTime dateReturn)
+        {
+            this.data.AddEvent(new Event(person, description, dateBorrow, dateReturn));
+        }
+
+
+
+        public List<Event> FindEvent(string enquiry)
+        {
+            List<Event> all = this.data.GetAllEvents().ToList<Event>();
+            List<Event> new1 = new List<Event>();
+            String text = string.Empty;
+
+            for (int i = 0; i < this.data.GetAllEvents().Count(); i++)
+            {
+                text = all[i].All;
+
+                if (text.Contains(enquiry))
+                {
+                    new1.Add(all[i]);
+                }
+            }
+
+            return new1;
+        }
+
+
+        public Dictionary<int, Catalog> FindCatalog(string enquiry)
+        {
+            Dictionary<int, Catalog> all = this.data.GetAllFromCatalog().ToDictionary(x => x.BookId, x => x);
+            Dictionary<int, Catalog> new1 = new Dictionary<int, Catalog>();
+
+            String text = string.Empty;
+            int index = 0;
+
+            for (int i = 0; i < this.data.GetAllRegisters().Count(); i++)
+            {
+                text = all[i].All;
+
+                if (text.Contains(enquiry))
+                {
+                    new1.Add(index, all[i]);
+                    index++;
+                }
+            }
+
+            return new1;
+        }
+
+
+        public List<StatusDescription> FindStatusDescription(double minimum, double maximum)
+        {
+            List<StatusDescription> all = this.data.GetAllStatusDescriptions().ToList();
+            List<StatusDescription> new1 = new List<StatusDescription>();
+
+            for (int i = 0; i < this.data.GetAllRegisters().Count(); i++)
+            {
+                if (all[i].Price > minimum && all[i].Price < maximum)
+                {
+                    new1.Add(all[i]);
+                }
+            }
+
+            return new1;
+        }
+
+
+        public ObservableCollection<Event> FindEvent(DateTime dateStart, DateTime dateEnd)
+        {
+            ObservableCollection<Event> all = new ObservableCollection<Event>(this.data.GetAllEvents());
+            ObservableCollection<Event> new1 = new ObservableCollection<Event>();
+
+            for (int i = 0; i < this.data.GetAllRegisters().Count(); i++)
+            {
+                if (all[i].DateBorrow > dateStart && all[i].DateReturn < dateEnd)
+                {
+                    new1.Add(all[i]);
+                }
+            }
+
+            return new1;
+        }
+    }
+}
