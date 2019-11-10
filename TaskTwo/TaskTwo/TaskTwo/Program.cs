78a2d925f538ce7266e61c37b5b990238430cf9d
Marcin <bee4004@gmail.com>
2019-11-10 17:18:51 +0100

    fix: showing data

--- TaskTwo/TaskTwo/TaskTwo/Program.cs
+++ TaskTwo/TaskTwo/TaskTwo/Program.cs
@@ -15,26 +15,31 @@ namespace TaskTwo
             Console.WriteLine("=========================================================================");
             Console.WriteLine("Welcome in simple TUI which allows You to perfom I/O operations on files.");
             Console.WriteLine("=========================================================================\n");
-            Console.WriteLine("[1] Fill with defined data");
+            Console.WriteLine("[1] Fill with defined data and show");
             Console.WriteLine("[2] Export to .json");
             Console.WriteLine("[3] Export to .dat");
             Console.WriteLine("[4] Import from .json");
             Console.WriteLine("[5] Import from .dat");
-            Console.WriteLine("[6] Show data");
-            Console.WriteLine("[7] Exit program");
+            Console.WriteLine("[6] Exit program");
 
 
             int choose = 0;
 
 
-            while (choose != 7)
+            while (choose != 6)
             {
                 Console.Write("\nEnter your choose: ");
-                choose = Console.Read()-'0';
+                choose = Console.Read() - '0';
                 switch (choose)
                 {
                     case 1:
-                        Console.WriteLine("Filling with defined data");
+                        Console.WriteLine("Filling with defined data\n");
+                        FillWithDefinedData dd = new FillWithDefinedData();
+                        DataService service = new DataService(dd.data);
+                        Console.WriteLine("Showing data:\n");
+                        service.View(dd.data.GetAllRegisters());
+                        service.View(dd.data.GetAllFromCatalog());
+                        service.View(dd.data.GetAllStatusDescriptions());
                         break;
 
                     case 2:
@@ -54,15 +59,6 @@ namespace TaskTwo
                         break;
 
                     case 6:
-                        Console.WriteLine("Showing data:\n");
-                        DataService service = new DataService(dd.data);
-                        service.View(dd.data.GetAllRegisters());
-                        service.View(dd.data.GetAllFromCatalog());
-                        service.View(dd.data.GetAllStatusDescriptions());
-
-                        break;
-
-                    case 7:
                         break;
 
                     default:
