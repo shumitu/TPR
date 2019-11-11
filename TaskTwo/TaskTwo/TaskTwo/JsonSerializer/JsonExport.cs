017bde298de1bd682ac26925a9cbbad36c90d8cc
Shumitu <darkdragon5255@gmail.com>
2019-11-11 15:19:11 +0100

    fix: fixed missing .json files

--- TaskTwo/TaskTwo/TaskTwo/JsonSerializer/JsonExport.cs
+++ TaskTwo/TaskTwo/TaskTwo/JsonSerializer/JsonExport.cs
@@ -13,57 +13,64 @@ using Task_1.Part_1;
         static public void SerializeRegister(DataRepository data)
         {
         JsonSerializer serializer = new JsonSerializer();
-        FileStream file1 = new FileStream("..\\..\\Files\\Register.json", FileMode.Create, FileAccess.Write);
+        FileStream file = new FileStream("..\\..\\Files\\Register.json", FileMode.Create, FileAccess.Write);
         IEnumerable<Register> constant = data.GetAllRegisters();
         string json = JsonConvert.SerializeObject(constant, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-        StreamWriter writer = new StreamWriter(file1);
+        StreamWriter writer = new StreamWriter(file);
         //JsonWriter jsonWriter = new JsonTextWriter(writer);
         //serializer.Serialize(jsonWriter, constant, typeof(DataContext));
         writer.WriteLine(json);
-        file1.Close();
-        Console.WriteLine(json);
+        writer.Flush();
+        writer.Close();
+        file.Close();
     }
 
 
         static public void SerializeCatalog(DataRepository data)
         {
             JsonSerializer serializer = new JsonSerializer();
-            FileStream file2 = new FileStream("..\\..\\Files\\Catalog.json", FileMode.Create, FileAccess.Write);
+            FileStream file = new FileStream("..\\..\\Files\\Catalog.json", FileMode.Create, FileAccess.Write);
             IEnumerable<Catalog> constant = data.GetAllFromCatalog();
             string json = JsonConvert.SerializeObject(constant, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-            StreamWriter writer = new StreamWriter(file2);
+            StreamWriter writer = new StreamWriter(file);
             //JsonWriter jsonWriter = new JsonTextWriter(writer);
             //serializer.Serialize(jsonWriter, constant, typeof(DataContext));
             writer.WriteLine(json);
-            file2.Close();
+            writer.Flush();
+            writer.Close();
+            file.Close();
         }
 
 
         static public void SerializeStatusDescription(DataRepository data)
         {
             JsonSerializer serializer = new JsonSerializer();
-            FileStream file3 = new FileStream("..\\..\\Files\\StatusDescription.json", FileMode.Create, FileAccess.Write);
+            FileStream file = new FileStream("..\\..\\Files\\StatusDescription.json", FileMode.Create, FileAccess.Write);
             IEnumerable<StatusDescription> constant = data.GetAllStatusDescriptions();
             string json = JsonConvert.SerializeObject(constant, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-            StreamWriter writer = new StreamWriter(file3);
+            StreamWriter writer = new StreamWriter(file);
             //JsonWriter jsonWriter = new JsonTextWriter(writer);
             //serializer.Serialize(jsonWriter, constant, typeof(DataContext));
             writer.WriteLine(json);
-            file3.Close();
+            writer.Flush();
+            writer.Close();
+            file.Close();
     }
 
 
         static public void SerializeEvent(DataRepository data)
         {
             JsonSerializer serializer = new JsonSerializer();
-            FileStream file4 = new FileStream("..\\..\\Files\\Event.json", FileMode.Create, FileAccess.Write);
+            FileStream file = new FileStream("..\\..\\Files\\Event.json", FileMode.Create, FileAccess.Write);
             IEnumerable<Event> constant = data.GetAllEvents();
             string json = JsonConvert.SerializeObject(constant, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-            StreamWriter writer = new StreamWriter(file4);
+            StreamWriter writer = new StreamWriter(file);
             //JsonWriter jsonWriter = new JsonTextWriter(writer);
             //serializer.Serialize(jsonWriter, constant, typeof(DataContext));
             writer.WriteLine(json);
-            file4.Close();
+            writer.Flush();
+            writer.Close();
+            file.Close();
         }
 
 
