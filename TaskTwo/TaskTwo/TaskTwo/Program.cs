096a2b8688d100764c7aef7c9f2ea10319171e5b
Marcin <bee4004@gmail.com>
2019-11-16 16:12:20 +0100

    fix: menu fixes

--- TaskTwo/TaskTwo/TaskTwo/Program.cs
+++ TaskTwo/TaskTwo/TaskTwo/Program.cs
@@ -14,15 +14,28 @@ namespace TaskTwo
             Console.WriteLine("=========================================================================");
             Console.WriteLine("Welcome in simple TUI which allows You to perform I/O operations on files.");
             Console.WriteLine("=========================================================================\n");
-            Console.WriteLine("[1] Fill with defined data");
-            Console.WriteLine("[2] Export to .json");
-            Console.WriteLine("[3] Export to .dat");
-            Console.WriteLine("[4] Import from .json");
-            Console.WriteLine("[5] Import from .dat");
-            Console.WriteLine("[6] Show data");
-            Console.WriteLine("[7] Clear data");
-            Console.WriteLine("[8] Exit program");
 
+            void Show()
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("[1] Fill with defined data");
+                Console.WriteLine("[2] Export to .json");
+                Console.WriteLine("[3] Export to .json (whole Context)");
+                Console.WriteLine("[4] Export to .dat");
+                Console.WriteLine("[5] Export to .dat (whole Context)");
+                Console.WriteLine("[6] Import from .json");
+                Console.WriteLine("[7] Import from .json (whole Context)");
+                Console.WriteLine("[8] Import from .dat");
+                Console.WriteLine("[9] Import from .dat (whole Context)");
+                Console.WriteLine("[10] Show data");
+                Console.WriteLine("[11] Clear data");
+                Console.WriteLine("[12] Exit program");
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+
+            Show();
 
             int choose = 0;
 
@@ -31,49 +44,78 @@ namespace TaskTwo
             DataRepository data = new DataRepository(empty);
 
 
-            while (choose != 8)
+            while (choose != 12)
             {
                 Console.Write("\nEnter your choose: ");
-                choose = Console.Read() - '0';
+                choose = Convert.ToInt32(Console.ReadLine());
+
                 switch (choose)
                 {
                     case 1:
                         Console.WriteLine("Filling with defined data");
                         data = new DataRepository(new DefinedData());
+                        Show();
                         break;
 
                     case 2:
                         Console.WriteLine("Exporting to .json");
                         JsonExport JsonExporter = new JsonExport();
-                        JsonContextSerialization whole = new JsonContextSerialization(data);
-                        whole.SerializeWhole();
                         JsonExporter.SerializeRegister(data);
                         JsonExporter.SerializeCatalog(data);
                         JsonExporter.SerializeStatusDescription(data);
                         JsonExporter.SerializeEvent(data);
+                        Show();
                         break;
 
                     case 3:
+                        Console.WriteLine("Exporting to .json (whole Context)");
+                        JsonContextSerialization whole = new JsonContextSerialization(data);
+                        whole.SerializeWhole();
+                        Show();
+                        break;
+
+                    case 4:
                         Console.WriteLine("Exporting to .dat");
                         OurExport OurExporter = new OurExport();
                         OurExporter.SerializeRegister(data);
                         OurExporter.SerializeCatalog(data);
                         OurExporter.SerializeStatusDescription(data);
                         OurExporter.SerializeEvent(data);
-                        break;
-
-                    case 4:
-                        Console.WriteLine("Importing from .json");
-                        data = new DataRepository(new JsonImport());
-                        data = new DataRepository(new JsonContextSerialization());
+                        Show();
                         break;
 
                     case 5:
-                        Console.WriteLine("Importing from .dat");
-                        data = new DataRepository(new OurImport());
+                        Console.WriteLine("Exporting to .dat (whole Context)");
+                        OurContextSerialization wholecontext = new OurContextSerialization(data);
+                        wholecontext.SerializeWhole();
+                        Show();
                         break;
 
                     case 6:
+                        Console.WriteLine("Importing from .json");
+                        data = new DataRepository(new JsonImport());
+                        Show();
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Importing from .json (whole Context)");
+                        data = new DataRepository(new JsonContextSerialization());
+                        Show();
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Importing from .dat");
+                        data = new DataRepository(new OurImport());
+                        Show();
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Importing from .dat (whole Context)");
+                        data = new DataRepository(new OurContextSerialization());
+                        Show();
+                        break;
+
+                    case 10:
                         Console.WriteLine("Showing data\n");
                         DataService service = new DataService(data);
                         Console.WriteLine("\nCatalogs:\n");
@@ -84,23 +126,25 @@ namespace TaskTwo
                         service.View(data.GetAllStatusDescriptions());
                         Console.WriteLine("\nEvents:\n");
                         service.View(data.GetAllEvents());
+                        Show();
                         break;
 
-                    case 7:
+                    case 11:
                         Console.WriteLine("Clearing data");
                         EmptyData empty2 = new EmptyData();
                         data = new DataRepository(empty2);
+                        Show();
                         break;
 
-                    case 8:
+                    case 12:
                         Environment.Exit(0);
                         break;
 
                     default:
+                        Console.WriteLine("Wrong choose");
+                        Show();
                         break;
                 }
-
-                Console.ReadLine();
             }
         }
     }
