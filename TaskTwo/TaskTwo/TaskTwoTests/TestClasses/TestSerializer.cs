13d3a0b7eef014fd7b01a6c0f510f045babd4059
Marcin <bee4004@gmail.com>
2019-11-20 20:24:09 +0100

    refactor: classes for recursive test

---
+++ TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTwoTests.TestClasses
+{
+    class TestSerializer
+    {
+    }
+}
