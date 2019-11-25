9426d057c69ade598b3b4df17d16698ca8aa0958
Marcin <bee4004@gmail.com>
2019-11-25 22:57:37 +0100

    refactor: TestClass

--- TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestClass.cs
+++ TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace TaskTwoTests.TestClasses
 {
@@ -6,6 +7,35 @@ namespace TaskTwoTests.TestClasses
     public class TestClass
     {
         public TestClass AnotherTestClass { get; set; }
-        public int Id { get; set; }
+        public float Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+
+        public TestClass(TestClass anotherTestClass, float id, DateTime date, string text)
+        {
+            AnotherTestClass = anotherTestClass;
+            Id = id;
+            Date = date;
+            Text = text;          
+        }
+
+
+        public TestClass(SerializationInfo info, StreamingContext context)
+        {
+            AnotherTestClass = (TestClass)info.GetValue("AnotherTestClass", typeof(TestClass));
+            Id = info.GetSingle("Id");
+            Date = info.GetDateTime("Date");
+            Text = info.GetString("Text");            
+        }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("AnotherTestClass", AnotherTestClass, typeof(TestClass));
+            info.AddValue("Id", Id);
+            info.AddValue("Date", Date);
+            info.AddValue("Text", Text);
+        }
     }
 }
