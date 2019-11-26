829e6eace2d6b39ed61fce22531d62d55091dec5
Marcin <bee4004@gmail.com>
2019-11-26 22:43:23 +0100

    Revert "refactor: TestClass"
    This reverts commit 9426d057c69ade598b3b4df17d16698ca8aa0958.

--- TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestClass.cs
+++ TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestClass.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace TaskTwoTests.TestClasses
 {
@@ -7,35 +6,6 @@ namespace TaskTwoTests.TestClasses
     public class TestClass
     {
         public TestClass AnotherTestClass { get; set; }
-        public float Id { get; set; }
-        public DateTime Date { get; set; }
-        public string Text { get; set; }
-
-
-        public TestClass(TestClass anotherTestClass, float id, DateTime date, string text)
-        {
-            AnotherTestClass = anotherTestClass;
-            Id = id;
-            Date = date;
-            Text = text;          
-        }
-
-
-        public TestClass(SerializationInfo info, StreamingContext context)
-        {
-            AnotherTestClass = (TestClass)info.GetValue("AnotherTestClass", typeof(TestClass));
-            Id = info.GetSingle("Id");
-            Date = info.GetDateTime("Date");
-            Text = info.GetString("Text");            
-        }
-
-
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("AnotherTestClass", AnotherTestClass, typeof(TestClass));
-            info.AddValue("Id", Id);
-            info.AddValue("Date", Date);
-            info.AddValue("Text", Text);
-        }
+        public int Id { get; set; }
     }
 }
---
+++ TaskTwo/TaskTwo/TaskTwoTests/TestClasses/TestSerializer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace TaskTwoTests.TestClasses
+{
+    public class TestSerializer
+    {
+        public ObjectIDGenerator generator = new ObjectIDGenerator();
+        public List<string[]> DeserializedData { get; set; }
+        private char DataSeparator = ';';
+
+        public TestSerializer()
+        {
+            DeserializedData = new List<string[]>();
+
+        }
+
+        public void Serialize(List<TestClass> listOfClasses, Stream inputStream)
+        {
+            using (StreamWriter writer = new StreamWriter(inputStream))
+            {
+                string toFile = "";
+                foreach (var singleClass in listOfClasses)
+                {
+                    long firstClassId = generator.GetId(singleClass, out bool firstTime);
+                    long otherClassId = generator.GetId(singleClass.AnotherTestClass, out firstTime);
+                    toFile += firstClassId;
+                    toFile += ";";
+                    toFile += otherClassId;
+                    toFile += ";";
+                    toFile += firstClassId;
+                    toFile += "\n";
+                }
+
+                toFile.Trim('\n');
+                Console.Write(toFile);
+                writer.Write(toFile);
+            }
+        }
+
+        public List<TestClass> Deserialize(Stream stream)
+        {
+            Dictionary<string, TestClass> classes = new Dictionary<string, TestClass>();
+            Dictionary<TestClass, string> secondClasses = new Dictionary<TestClass, string>();
+            List<TestClass> resultClasses = new List<TestClass>();
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var fileDataLine = "";
+                while ((fileDataLine = reader.ReadLine()) != null)
+                {
+                    char[] separator = { DataSeparator };
+                    DeserializedData.Add(fileDataLine.Split(separator));
+                }
+
+                foreach (string[] dataSet in DeserializedData)
+                {
+                    if (classes.ContainsKey(dataSet[0]))
+                    {
+                        TestClass deserializedClass = classes[dataSet[0]];
+                        resultClasses.Add(deserializedClass);
+                    }
+                    else
+                    {
+                        TestClass deserializedClass = new TestClass()
+                        {
+                            Id = int.Parse(dataSet[2])
+                        };
+                        if (classes.ContainsKey(dataSet[1]))
+                        {
+                            deserializedClass.AnotherTestClass = classes[dataSet[1]];
+                        }
+                        else
+                        {
+                            secondClasses.Add(deserializedClass, dataSet[1]);
+                        }
+                        classes.Add(dataSet[0], deserializedClass);
+                        resultClasses.Add(deserializedClass);
+                    }
+                }
+            }
+
+            foreach (TestClass resultClass in resultClasses)
+            {
+                if (resultClass.AnotherTestClass == null)
+                    resultClass.AnotherTestClass = classes[secondClasses[resultClass]];
+            }
+
+            return resultClasses;
+        }
+    }
+}
