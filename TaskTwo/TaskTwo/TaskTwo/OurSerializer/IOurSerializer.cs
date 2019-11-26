3479bcdeebcab1775bf27584327a0eea009af868
Marcin <bee4004@gmail.com>
2019-11-26 22:43:28 +0100

    Revert "refactor: OurSerializer"
    This reverts commit d3ea15239e5646b64331cc872f854d9df3eae858.

---
+++ TaskTwo/TaskTwo/TaskTwo/OurSerializer/IOurSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using Task_1.Part_1;
+
+namespace TaskTwo.OurSerializer
+{
+    public interface IOurSerializer
+    {
+        void Serialize(DataContext dataContext, Stream outputStream);
+        DataContext Deserialize(Stream inputStream);
+    }
+}
