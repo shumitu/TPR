9be64b66b151945d127668c494df50943454ac01
Shumitu <darkdragon5255@gmail.com>
2019-11-27 11:09:40 +0100

    refactor: third part of code refactoring

--- TaskTwo/TaskTwo/TaskTwo/OurNewSerializer/OurNewSerializer.cs
+++ TaskTwo/TaskTwo/TaskTwo/OurNewSerializer/OurNewSerializer.cs
@@ -18,53 +18,85 @@ namespace TaskTwo.OurNewSerializer
             Context = new StreamingContext(StreamingContextStates.File);
         }
 
-
-        public override object Deserialize(Stream serializationStream)
-        {
-            ReadDeserializeInfoAndCreateReferences(serializationStream);
-
-            foreach (string row in DeserializeInfo)
-            {
-                string[] splitDataRow = row.Split('|');
-                Type objectDeserializeType = GetTypeFromSplitDeserializeInfoRow(splitDataRow);
-                SerializationInfo info = new SerializationInfo(objectDeserializeType, new FormatterConverter());
-                GetSerializationInfoFromDeserializeInfoRow(info, splitDataRow);
-                Type[] constructorTypes = { info.GetType(), Context.GetType() };
-                object[] constuctorParameters = { info, Context };
-                References[splitDataRow[2]].GetType().GetConstructor(constructorTypes).Invoke(References[splitDataRow[2]], constuctorParameters);
-            }
-            return References["1"];
-        }
-
-
         public override void Serialize(Stream serializationStream, object desiredObjToSerialize)
         {
             if (desiredObjToSerialize is ISerializable data)
             {
                 SerializationInfo infoAboutObject = new SerializationInfo(desiredObjToSerialize.GetType(), new FormatterConverter());
                 Binder.BindToName(desiredObjToSerialize.GetType(), out string assemblyName, out string typeName);
-                DataRow += assemblyName + "|" + typeName + "|" + this.m_idGenerator.GetId(desiredObjToSerialize, out bool firstTime);
+
+                DataRow += assemblyName 
+                           + "|" + typeName
+                           + "|" + this.m_idGenerator.GetId(desiredObjToSerialize, out bool firstTime);
+
                 data.GetObjectData(infoAboutObject, Context);
 
-                foreach (SerializationEntry item in infoAboutObject)
+                foreach (SerializationEntry singleObjItem in infoAboutObject)
                 {
-                    WriteMember(item.Name, item.Value);
+                    WriteMember(singleObjItem.Name, singleObjItem.Value);
                 }
 
                 DataToSave.Add(DataRow + "\n");
                 DataRow = "";
 
-                while (this.m_objectQueue.Count != 0)
+                while (m_objectQueue.Count != 0)
                 {
-                    this.Serialize(null, this.m_objectQueue.Dequeue());
+                    Serialize(null, m_objectQueue.Dequeue());
                 }
 
-                SaveToStream(serializationStream);
+                // Saving to stream...
+
+                if (serializationStream != null)
+                {
+                    using (StreamWriter writer = new StreamWriter(serializationStream))
+                    {
+                        foreach (string line in DataToSave)
+                        {
+                            writer.Write(line);
+                        }
+                    }
+                }
+
             }
             else
             {
                 throw new ArgumentException("Chosen desiredObjToSerialize is not Iserializable");
             }
+        }
+
+
+        public override object Deserialize(Stream serializationStream)
+        {
+            if (serializationStream != null)
+            {
+                using (StreamReader reader = new StreamReader(serializationStream))
+                {
+                    String line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        DeserializeInfo.Add(line);
+                    }
+                }
+
+                foreach (string item in DeserializeInfo)
+                {
+                    String[] splits = item.Split('|');
+                    References.Add(splits[2], FormatterServices.GetSafeUninitializedObject(Binder.BindToType(splits[0], splits[1])));
+                }
+            }
+
+            foreach (string singleDataRow in DeserializeInfo)
+            {
+                string[] dividedDataRow = singleDataRow.Split('|');
+                Type objectDeserializeType = Binder.BindToType(dividedDataRow[0], dividedDataRow[1]);
+                SerializationInfo info = new SerializationInfo(objectDeserializeType, new FormatterConverter());
+                GetSerializationInfoFromDeserializeInfoRow(info, dividedDataRow);
+                Type[] constructorTypes = { info.GetType(), Context.GetType() };
+                object[] constuctorParameters = { info, Context };
+                References[dividedDataRow[2]].GetType().GetConstructor(constructorTypes).Invoke(References[dividedDataRow[2]], constuctorParameters);
+            }
+            return References["1"];
         }
 
 
@@ -225,49 +257,6 @@ namespace TaskTwo.OurNewSerializer
         private List<string> DataToSave = new List<string>();
         private List<string> DeserializeInfo = new List<string>();
         private Dictionary<string, object> References = new Dictionary<string, object>();
-
-
-        private void SaveToStream(Stream serializationStream)
-        {
-            if (serializationStream != null)
-            {
-                using (StreamWriter writer = new StreamWriter(serializationStream))
-                {
-                    foreach (string line in DataToSave)
-                    {
-                        writer.Write(line);
-                    }
-                }
-            }
-        }
-
-
-        private void ReadDeserializeInfoAndCreateReferences(Stream serializationStream)
-        {
-            if (serializationStream != null)
-            {
-                using (StreamReader reader = new StreamReader(serializationStream))
-                {
-                    String line;
-
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        DeserializeInfo.Add(line);
-                    }
-                }
-                CreateReferences();
-            }
-        }
-
-
-        private void CreateReferences()
-        {
-            foreach (string item in DeserializeInfo)
-            {
-                String[] splits = item.Split('|');
-                References.Add(splits[2], FormatterServices.GetSafeUninitializedObject(Binder.BindToType(splits[0], splits[1])));
-            }
-        }
 
 
         private Type GetTypeFromSplitDeserializeInfoRow(string[] splitDeserializeRow)
