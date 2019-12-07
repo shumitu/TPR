0647767bd36b9b9928b43f9ee2c23a754c6fc38c
Shumitu <darkdragon5255@gmail.com>
2019-12-07 18:51:26 +0100

    add: first test of extended methods

---
+++ TaskThree/TaskThree/TaskThreeTests/ExtendingSqlToolsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskThree.Classes;
+using TaskThree.Files;
+
+namespace TaskThreeTests
+{
+    [TestClass]
+    public class ExtendingSqlToolsTest
+    {
+        [TestMethod]
+        public void ListProductsWithoutCategory()
+        {
+            using (DataDataContext dataContext = new DataDataContext())
+            {
+                List<Product> productsList = dataContext.GetTable<Product>().ToList();
+                List<Product> resultList = productsList.GetProductsWithoutAnyCategory();
+
+                foreach (var singleResult in resultList)
+                {
+                    Assert.AreEqual(singleResult.ProductSubcategory, null);
+                }
+
+                Assert.AreEqual(resultList.Count, 209);
+            }
+
+            // to be added...
+        }
+    }
+}
